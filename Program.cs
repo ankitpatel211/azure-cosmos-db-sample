ae8c42e2339d6e875bf5bf26857dafead4d01599
Ankit Patel <wd.ankit.patel@gmail.com>
2024-05-21 15:06:19 +0530

    Updated configuration values like endpoint, key, database, and container

--- Program.cs
+++ Program.cs
@@ -5,15 +5,15 @@ CosmosClientOptions options = new()
     ConnectionMode = ConnectionMode.Direct,
     ConsistencyLevel = ConsistencyLevel.Eventual
 };
-string endpoint = "";
-string key = "";
+string endpoint = "<cosmos-endpoint>";
+string key = "<cosmos-key>";
 CosmosClient client = new (endpoint, key, options);
 AccountProperties account = await client.ReadAccountAsync();
 Console.WriteLine($"Cosmos Account Name:\t{account.Id}");
 Console.WriteLine($"Cosmos Primary Region:\t{account.WritableRegions.FirstOrDefault()?.Name}");
-string databaseId = "cosmicworks";
+string databaseId = "<cosmos-database>";
 Database database = await client.CreateDatabaseIfNotExistsAsync(databaseId);
 Console.WriteLine($"Cosmos Database:\t{database.Id}");
-string containerId = "task";
-Container container = await database.CreateContainerIfNotExistsAsync(containerId,"/taskId",400);
+string containerId = "<cosmos-container>";
+Container container = await database.CreateContainerIfNotExistsAsync(id:containerId,partitionKeyPath:"/<partitionKeyPath>",throughput:400);
 Console.WriteLine($"Cosmos Container:\t{container.Id}");
