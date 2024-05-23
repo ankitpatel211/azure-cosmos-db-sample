1a5d30f03935b39a7fe19129654e86d3a327acac
Ankit Patel <wd.ankit.patel@gmail.com>
2024-05-23 14:56:50 +0530

    Upgrading code

--- Program.cs
+++ Program.cs
@@ -1,29 +1,49 @@
-﻿using Microsoft.Azure.Cosmos;
-using Microsoft.Extensions.Configuration;
+﻿#region Packages
+    using Microsoft.Azure.Cosmos;
+    using Microsoft.Extensions.Configuration;
+#endregion
 
-var builder = new ConfigurationBuilder();
-builder.SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-IConfiguration config = builder.Build();
+#region Application configuration
+    //Build configuration using a Json configuration file
+    var builder = new ConfigurationBuilder();
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true);
+    var config = builder.Build();
+#endregion
 
-CosmosClientOptions options = new()
-{
-    ApplicationPreferredRegions = new List<string>() { "WestUS", "EastUS" },
-    ConnectionMode = ConnectionMode.Direct,
-    ConsistencyLevel = ConsistencyLevel.Eventual
-};
-var endpoint = config["cosmosConnection:endpoint"];
-var key = config["cosmosConnection:key"];
-CosmosClient client = new(endpoint, key, options);
-AccountProperties account = await client.ReadAccountAsync();
-Console.WriteLine($"Cosmos Account Name:\t{account.Id}");
-Console.WriteLine($"Cosmos Primary Region:\t{account.WritableRegions.FirstOrDefault()?.Name}");
-var databaseId = config["cosmosConnection:database:id"];
-Database database = await client.CreateDatabaseIfNotExistsAsync(databaseId);
-Console.WriteLine($"Cosmos Database:\t{database.Id}");
-var containerId = config["cosmosConnection:database:containers:firstContainer:id"];
-var partitionKey = config["cosmosConnection:database:containers:firstContainer:partitionKey"];
-var throughputValue = config["cosmosConnection:database:containers:firstContainer:throughput"];
-Container container = await database.CreateContainerIfNotExistsAsync(id: containerId,
-    partitionKeyPath: "/" + partitionKey + "", throughput: Convert.ToInt32(throughputValue));
-Console.WriteLine($"Cosmos Container:\t{container.Id}");
+#region Cosmos Connection
+    //Cosmos client options
+    CosmosClientOptions options = new()
+    {
+        ApplicationPreferredRegions = new List<string>() { "WestUS", "EastUS" },
+        ConnectionMode = ConnectionMode.Direct,
+        ConsistencyLevel = ConsistencyLevel.Eventual
+    };
+    // Connection cofiguration
+    var endpoint = config["cosmos:connection:endpoint"];
+    var key = config["cosmos:connection:key"];
+    CosmosClient client = new (endpoint, key, options);
+    // Account properties
+    AccountProperties account = await client.ReadAccountAsync();
+    Console.WriteLine($"Cosmos Account Name:\t{account.Id}");
+    Console.WriteLine($"Cosmos Primary Region:\t{account.WritableRegions.FirstOrDefault()?.Name}");
+#endregion
+
+#region Cosmos Database
+    // Database configuration
+    var databaseId = config["cosmos:database:id"];
+    // Create a new database
+    Database database = await client.CreateDatabaseIfNotExistsAsync(databaseId);
+    Console.WriteLine($"Cosmos Database:\t{database.Id}");
+#endregion
+
+#region Cosmos Container
+    // Container configuration
+    var containerId = config["cosmos:database:container:id"];
+    var partitionKey = config["cosmos:database:container:partitionKey"];
+    var throughputValue = config["cosmos:database:container:throughput"];
+    // Create a new container
+    Container container = await database.CreateContainerIfNotExistsAsync(id: containerId, 
+      partitionKeyPath: partitionKey, throughput: Convert.ToInt32(throughputValue));
+    Console.WriteLine($"Cosmos Container:\t{container.Id}");
+#endregion
